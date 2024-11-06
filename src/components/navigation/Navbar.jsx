242cf5ea308c1d0d2a3e7a505d5010aa092c5de4
Ryan <Ryan.Bee.Dev@outlook.com>
2024-11-06 18:27:25 -0500

    Bootstrap + Navigation
    add bootstrap/sass + redo navbar as offcanvas

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "@/styles/navbar.css";
 
 import HomeIcon from "@/assets/icon/homeIcon.svg";
 import TaskIcon from "@/assets/icon/taskIcon.svg";
@@ -13,34 +12,36 @@ import LogoutIcon from "@/assets/icon/logoutIcon.svg";
 export const Navbar = () => {
     return (
         <>
-            <div className="nav">
-                <div className="navMenu">
-                    <NavLink className="navLink" to="/">
-                        <img className="icon" src={HomeIcon}></img>
+            <div className="container-fluid h-100">
+                <div className="navbar-nav h-50">
+                    <NavLink className="p-4 nav-link" to="/">
+                        <img className="p-2" src={HomeIcon}></img>
                         Home
                     </NavLink>
-                    <NavLink className="navLink" to="/tasks">
-                        <img className="icon" src={TaskIcon}></img>
+                    <NavLink className="p-4 nav-link" to="/tasks">
+                        <img className="p-2" src={TaskIcon}></img>
                         Task Manager
                     </NavLink>
-                    <NavLink className="navLink" to="/orders">
-                        <img className="icon" src={OrderIcon}></img>
+                    <NavLink className="p-4 nav-link" to="/orders">
+                        <img className="p-2" src={OrderIcon}></img>
                         Orders
                     </NavLink>
-                    <NavLink className="navLink" to="/customers">
-                        <img className="icon" src={ClientsIcon}></img>
+                    <NavLink className="p-4 nav-link" to="/customers">
+                        <img className="p-2" src={ClientsIcon}></img>
                         Clients
                     </NavLink>
-                    <NavLink className="navLink" to="/products">
-                        <img className="icon" src={CatalogIcon}></img>
+                    <NavLink className="p-4 nav-link" to="/products">
+                        <img className="p-2" src={CatalogIcon}></img>
                         Catalog
                     </NavLink>
-                    <NavLink className="navLink" to="/about">
-                        <img className="icon" src={AboutIcon}></img>
+                    <NavLink className="p-4 nav-link" to="/about">
+                        <img className="p-2" src={AboutIcon}></img>
                         About
                     </NavLink>
-                    <NavLink className="navLink" to="/logout">
-                        <img className="icon" src={LogoutIcon}></img>
+                </div>
+                <div className="navbar-nav h-50 justify-content-end">
+                    <NavLink className="p-4 nav-link" to="/logout">
+                        <img className="p-2" src={LogoutIcon}></img>
                         Log Out
                     </NavLink>
                 </div>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,40 +1,80 @@
-import React from 'react';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+
+//////////////////////////////////////////
+//Assets
+//////////////////////////////////////////
+import ReactLogo from "@/assets/icon/reactlogo.png";
+import MenuIcon from "@/assets/icon/menu.svg";
+import CloseIcon from "@/assets/icon/closeIcon.svg";
+
+//////////////////////////////////////////
+//Components
+//////////////////////////////////////////
 import { Navbar } from "@/components/navigation/Navbar";
-import reactLogo from "@/assets/icon/reactlogo.png";
+
+//////////////////////////////////////////
+//Bootstrap
+//////////////////////////////////////////
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function Layout() {
+  const [showNav, setNav] = useState(false);
+
+  const handleCloseNav = () => setNav(false);
+  const handleShowNav = () => setNav(true);
+
   return (
     <>
-        <div className="site-container">
-        <div className="titlebar"></div>
-        <div className="header r1 c1">
+
+      <div className="sticky-top p-2 d-flex flex-nowrap align-items-baseline text-white bg-dark">
+        <div className="display-1 col-sm-2 text-center">
           NW
         </div>
-        <div className="header r1 c2">
+        <div className="display-4 col-sm-8 text-left">
           Northwind
         </div>
         
-        <div className="header r1 c3">
-            Powered by React 
-            <img className="logo" src={reactLogo}></img>
+        {/*<div className="h4 col-sm-2 text-right">
+          <div class="d-flex flex-nowrap align-items-baseline justify-content-end">
+            Powered by React
+            <img className="react-logo" src={ReactLogo}></img>
+          </div>
         </div>
+        */}
 
-        <div className="box sidebar">
+        <div className="col-sm-2">
+          <button className="btn-default" type="button" onClick={handleShowNav}>
+            <img src={MenuIcon}></img>
+          </button>
+        </div>
+      </div>
+
+      <Offcanvas className="bg-black" show={showNav} onHide={handleCloseNav}>
+        <Offcanvas.Header className="justify-content-between">
+          <div className="display-1 text-white">Menu</div>
+          <div>
+            <button className="btn-default" type="button" onClick={handleCloseNav}>
+              <img src={CloseIcon}></img>
+            </button>
+          </div>
+          
+        </Offcanvas.Header>
+        <Offcanvas.Body>
           <Navbar />
-        </div>
+        </Offcanvas.Body>
+      </Offcanvas>
 
-        <div className="box content">
-          <Outlet />
-        </div>
+      <div className="box content">
+        <Outlet />
+      </div>
 
-        <div className="box footer">
-          footer
-        </div>
+      <div className="box footer">
+        footer
+      </div>
 
-        <div className="box footer">
-          footer
-        </div>
+      <div className="box footer">
+        footer
       </div>
     </>
   );
