4053d5215ced4a6fb38c7aa82936cc62b6d8e95d
Ryan <Ryan.Bee.Dev@outlook.com>
2026-04-24 15:17:43 -0400

    Set  Vite Base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/NorthWindWeb/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
